277a96f6b9cac4da23aea8a26c46f7e5a54a5e6f
Raphael Carubbi Neto <rcarubbi@gmail.com>
2018-01-02 07:54:03 -0200

    limit request size, enable cors

--- src/app.js
+++ src/app.js
@@ -22,10 +22,20 @@ const productRoute = require('./routes/product-route');
 const customerRoute = require('./routes/customer-route');
 const orderRoute = require('./routes/order-route');
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({ 
+    limit: '10mb'
+}));
 app.use(bodyParser.urlencoded({ 
     extended: false
 }));
+
+// enable cors
+app.use(function(req, res, next){
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, x-access-token");
+    res.header("Access-Control-Allow-Methods","GET, POST, PUT, DELETE, OPTIONS");
+    next();
+});
 
 // bind routes to express
 app.use('/', indexRoute);
